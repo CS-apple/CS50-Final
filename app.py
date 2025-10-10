4f620c100b9e805e7ce5e489456c78026afca263
cs-apple <dimitri.dietz93@gmail.com>
2025-10-09 21:25:33 -0400

    updated file structure

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template, request, redirect, session
 from cs50 import SQL
 
 #configure app 
@@ -9,6 +9,7 @@ app = Flask(__name__)
 #configure db
 db = SQL("sqlite:///test.db")
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("index.html")

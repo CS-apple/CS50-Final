722be952c507148839691a3555cf5bf18679181c
cs-apple <dimitri.dietz93@gmail.com>
2025-10-29 22:57:57 -0400

    addint log in and register

--- static/tools.js
+++ static/tools.js
@@ -1,6 +1,57 @@
 console.log("Hello from tools.js");
+//fill box with 12 doughnuts
+const boxSize = 12;
+//object box
+const box ={  
+ contents: new Array(boxSize)
+};
+//a box is an array od doughnut objects
+const doughnut = {
+    name: "",
+};
 
-//Create ticker that enables buttons when doughnut total is 12
+function val() {
+    const doughnut1 = document.getElementById('option1');
+    console.log(box.contents);
+    for (i of box.contents) {
+        if (i == undefined) {
+            box.contents[i] = doughnut1;
+            console.log(box.contents[i]);
+        }
+        else {
+            break;
+        } 
+    }
+    for (j in box) {
+        console.log(box.contents[j]);
+    }
+ }
+
+
+//why do we need to do this, why dotn we just check all the inputs total to 12 then fill box on submission 
+
+
+//when user increments add doughnut to box 
+//when user decrements remove doughnut from box
+
+
+
+
+//a box must equal 12 doughnuts 
+//customers can only order a full box
+
+
+//When the user changes the input value, recalculate the total number of doughnuts
+//If the total number of doughnuts is 12, enable the buttons
+//If the total number of doughnuts is not 12, show message and disable the buttons
+
+//when user submits form, add to cart
+//show confirmation message
+//hide confirmation message when user clicks on it
+
+
+
+/*//Create ticker that enables buttons when doughnut total is 12
 const doughnut1 = document.getElementById('option1');
 const doughnut2 = document.getElementById('option2');
 const doughnut3 = document.getElementById('option3');
@@ -49,4 +100,4 @@ doughnut2.addEventListener('input', function() {
 doughnut3.addEventListener('input', function() {
     CalcTotal();
     UpdateButtons();
-})
+})*/

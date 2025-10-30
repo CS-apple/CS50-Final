a5565576c8963177415edace6b289ee33a4f08c5
cs-apple <dimitri.dietz93@gmail.com>
2025-10-30 00:18:44 -0400

    comppleted login and register pages

--- app.py
+++ app.py
@@ -9,15 +9,38 @@ app = Flask(__name__)
 #configure db
 db = SQL("sqlite:///test.db")
 
+#routes
+
+#HOME
 @app.route("/", methods = ["GET", "POST"])
 def index():
     if request.method == "GET":
         return render_template("index.html")
 
+#ORDER
 @app.route("/order", methods = ["GET", "POST"])
 def order():
     if request.method == "GET":
         return render_template("order.html")
     
+#LOGIN    
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")  
+
+#REGISTER
+@app.route("/register", methods = ["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+
+#ADMIN
+@app.route("/admin", methods = ["GET", "POST"])
+def admin():
+    if request.method == "GET":
+        return render_template("admin.html")
+    
+
 if __name__ == "__main__":
     app.run(debug=True)

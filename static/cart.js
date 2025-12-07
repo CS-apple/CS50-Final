4aeb4634624874d6ec9fe06dafa648e44948e415
cs-apple <dimitri.dietz93@gmail.com>
2025-12-07 14:41:04 -0500

    display cart items

--- static/cart.js
+++ static/cart.js
@@ -1,3 +1,4 @@
+
 if (document.readyState == 'loading') {
     document.addEventListener('DOMContentLoaded', ready)
 } else {
@@ -7,11 +8,6 @@ if (document.readyState == 'loading') {
 function ready(){
     console.log("Hello from cart.js");
         //add event llisteners for edit and remove buttons
-    const cartOptions = document.getElementsByClassName("cart-btns");
-    const cartBtns = cartOptions[0].getElementsByClassName("btn");
-    for ( let i=0; i < cartBtns.length; i++){
-        cartBtns[i].addEventListener('click', cartFunctions)
-    };
     //add event listener for checkout button
     const checkoutButton = document.getElementById("checkout-btn");
     checkoutButton.addEventListener('click', Checkout)
@@ -65,13 +61,16 @@ function retrieveCart(){
     let cart =JSON.parse( sessionStorage.getItem("cart"));
     console.log("Cart items:", cart);
     //create cart objects
-    for (let i=0; i<cart.length;i++){
+    for (let i=0; i < cart.length; i++){
+        let j = i+1;
         const cartItem = new CartItem(
-             "Custom Box" + (i+1),
+             "Custom Box " + j,
             15.00,
             cart[i],
         );
-        displayCartItem(cartItem);    
+        console.log("Cart Item Object:", cartItem);
+        displayCartItem(cartItem);
+
     };
     } else {
         //show user their cart is empty
@@ -80,25 +79,47 @@ function retrieveCart(){
         <h5>Your cart is empty</h5>
         <p>would you like to <a href="/order">create an order</a>?</p>
         `;
-        let cartBody = getElementsbyCLassName('cart-body')
+        let cartBody = document.getElementsByClassName('card-body')
         cartBody[0].innerHTML= emptyCartHTML;
     };
 };
 
 //Display cart items in HTML
 function displayCartItem(cartRow){
-    const cart = document.getElementsByClassName('cart-row')[0];
-    const cartHeader = cart.getElementsByClassName('cart-body')[0];
+    const cart = document.getElementsByClassName('card-body')[0];
+    //create new row
+    const row= document.createElement('div');
+    row.classList.add('cartRow', 'row');
+    cart.appendChild(row);
+    //create col cart body, append
+    const colBody = document.createElement('div');
+    colBody.classList.add('cart-body', 'col', 'mb-3');
+    row.appendChild(colBody);
+    // insert box name and price 
+    colBody.appendChild(newBox(cartRow));
+    // create UL, flavor list,append
+    const flavorList = document.createElement('ul');
+    flavorList.classList.add('flavor-list', 'list-group', 'list-group-flush')
+    colBody.appendChild(flavorList); 
+    // populate with li for each favor in cartRow.items
+    for (let i=0; i <  cartRow.items.length; i++){
+        flavorList.appendChild(createFlavorItem(cartRow.items[i]));
+    };
+    //create cart buttons, append
+    //add event listeners to buttons 
+
+
+   /* const cartHeader = cart.getElementsByClassName('cart-body')[0];
         const cartHeaderHTML = `
         <div class ="Cart-item-header d-flex justify-content-between align-items-center">
-            <h5 class="name">Item Name</h5>
+            <h5 class="name"></h5>
             <h5 class="price">$15.00</h5>
         </div>
         `;
     cartHeader.innerHTML = cartHeaderHTML;
-
+    let boxBreakdown = cart.getElementsByClassName('flavor-list')[0];
+    console.log("flavor list: ", boxBreakdown);
     for (let i=0; i < cartRow.items.length; i++){
-        const boxBreakdown = cart.getElementsByClassName('flavor-list')[0];
         const flavor = `
                 <li class="flavor-item list-group-item d-flex justify-content-between align-items-center">
                     <p class="flavor mb-0">${cartRow.items[i].flavor}</p>
@@ -106,34 +127,63 @@ function displayCartItem(cartRow){
                 </li>                 
         `; 
         boxBreakdown.innerHTML = flavor;
-    }
-
-/*<div class ="Cart-item-header d-flex justify-content-between align-items-center">
-                        <h5 class="name">Item Name</h5>
-                        <h5 class="price">$15.00</h5>
-                    </div>
-                    <div class ="cart-item-body">
-                        <ul class="flavor-list list-group list-group-flush">
-                            <li class="flavor-item list-group-item d-flex justify-content-between align-items-center">
-                                <p class="flavor mb-0">Doughnut</p>
-                                <p class="quantity mb-0">3</p>
-                            </li>
-                            <li class="flavor-item list-group-item d-flex justify-content-between align-items-center">
-                                <p class="flavor mb-0">Doughnut</p>
-                                <p class="quantity mb-0">3</p>
-                            </li>
-                            <li class="flavor-item list-group-item d-flex justify-content-between align-items-center">
-                                <p class="flavor mb-0">Doughnut</p>
-                                <p class="quantity mb-0">3</p>
-                            </li>                   
-                        </ul>
-                    </div>
-                    <div class ="cart-btns mt-2 col-4 justify-content-end align-items-center">
-                        <button class="btn btn-secondary">Edit</button>
-                        <button class="btn btn-danger">Remove</button> 
-                    </div> */
-
+    }*/
 };
+function newBox(cartRow){
+    const boxInfo = document.createElement('div');
+        boxInfo.classList.add('Cart-item-header', 'd-flex', 'justify-content-between', 'align-items-center')
+        const boxInfoHTML = `                    
+            <h5 class="name">${cartRow.name}</h5>
+            <h5 class="price">${'$' + cartRow.price}</h5>
+            `;
+        boxInfo.innerHTML = boxInfoHTML;
+        return boxInfo;
+}; 
+
+function createFlavorItem(flavorData){
+    const boxItem = document.createElement('li');
+    boxItem.classList.add('flavor-item', 'list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
+    const flavorItem = `
+        <p class="flavor mb-0">${flavorData.flavor}</p>
+        <p class="quantity mb-0">${flavorData.quantity}</p>
+    `;
+    boxItem.innerHTML = flavorItem;
+    return boxItem; 
+};
+/*        
+    <div class="card-body">
+        <div class="cart-row row">
+            <div class="cart-body col mb-3">
+                <div class ="Cart-item-header d-flex justify-content-between align-items-center">
+                    <h5 class="name">Item Name</h5>
+                    <h5 class="price">$15.00</h5>
+                </div>
+                <div class ="cart-item-body">
+                    <ul class="flavor-list list-group list-group-flush">
+                        <li class="flavor-item list-group-item d-flex justify-content-between align-items-center">
+                            <p class="flavor mb-0">Doughnut</p>
+                            <p class="quantity mb-0">3</p>
+                        </li>
+                        <li class="flavor-item list-group-item d-flex justify-content-between align-items-center">
+                            <p class="flavor mb-0">Doughnut</p>
+                            <p class="quantity mb-0">3</p>
+                        </li>
+                        <li class="flavor-item list-group-item d-flex justify-content-between align-items-center">
+                            <p class="flavor mb-0">Doughnut</p>
+                            <p class="quantity mb-0">3</p>
+                        </li>                   
+                    </ul>
+                </div>
+                <div class ="cart-btns mt-2 col-4 justify-content-end align-items-center">
+                    <button class="btn btn-secondary">Edit</button>
+                    <button class="btn btn-danger">Remove</button> 
+                </div>   
+            </div>
+        </div>
+    </div>
+*/
+
+
 
 function updateTotalPrice(){
     console.log("price updated");

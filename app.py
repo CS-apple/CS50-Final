deb8843c1062e0e688767456652722bb5d0a3644
dimi <dimi@Dimitris-MacBook-Pro.local>
2026-01-20 00:59:30 -0500

    e

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ def register():
         )
         #insert password into password table 
         sessionID = db.execute(
-            'INSERT INTO hash (user_id, password_hash) VALUES (?,?)', userID, hash_password(request.form.get('password'), method='pbkdf2:sha256') 
+            'INSERT INTO hash (user_id, password_hash) VALUES (?,?)', userID, generate_password_hash(request.form.get('password'), method='pbkdf2:sha256') 
         )
         #session user id = sessions id 
         session["userID"] =  sessionID

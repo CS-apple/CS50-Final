a7bcd6bb3ccaf02c7dc35a95b779bec51835d5ee
dimi <dimi@Dimitris-MacBook-Pro.local>
2026-01-19 21:03:29 -0500

    working on register backend

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, flash, redirect, url_for
 from cs50 import SQL
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -16,6 +16,13 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 #routes
 
@@ -40,7 +47,53 @@ def login():
 #REGISTER
 @app.route("/register", methods = ["GET", "POST"])
 def register():
-    if request.method == "GET":
+    """Register user"""
+    if request.method == "POST":
+        # first name submitted? 
+        if not request.form.get('firstname'):
+            flash('Please add your first name', 'alert-danger')
+            return render_template("register.html")
+        # last name submitted?
+        if not request.form.get('lastname'):
+            return render_template("register.html", message="Must provide last name")
+        # phone number submitted?
+        if not request.form.get('phone'):
+            return render_template("register.html", message="Must provide phone number")
+        # email submitted?
+        if not request.form.get('email'):
+            return render_template("register.html", message="Must provide email")
+        # password submitted?
+        if not request.form.get('password'):
+            return render_template("register.html", message="Must provide password")
+        # confirm password submitted
+        if not request.form.get('confirmation'):
+            return render_template("register.html", message="Must confirm password")
+        # match password
+        if request.form.get('password') != request.form.get('confirmation'):
+            return render_template("register.html", message="Passwords do not match")
+        # query db for email
+        account = db.execute(
+            'SELECT * FROM users WHERE email = ?', request.form.get("email")
+        )
+        # check if user is already in db 
+        if len(account)!=0:
+            flash('Account already exists for this email address','alert-danger')
+            return render_template("register.html")
+        #register user in db 
+        userID = db.execute(
+            'INSERT INTO users (firstname,lastname,email, phone) VALUES (?,?,?,?)', request.form.get("firstname"),request.form.get("lastname"),request.form.get("phone"), request.form.get("email")
+        )
+        #insert password into password table 
+        sessionID = db.execute(
+            'INSERT INTO hash (user_id, password_hash) VALUES (?,?)', userID, generate_password_hash("request.form.get('password')") 
+        )
+        #session user id = sessions id 
+        session['userID'] =  sessionID
+        #if user has a cart in sessions storrage,
+        #redirect to homepage
+        return redirect("/")
+
+    else: 
         return render_template("register.html")
 
 #ADMIN

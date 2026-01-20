8ef8bc1689390011891216fa913caa343a293979
dimi <dimi@Dimitris-MacBook-Pro.local>
2026-01-20 00:41:17 -0500

    edit

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import os
-import hashlib
-import ssl
 
 from flask import Flask, render_template, request, redirect, session, flash, redirect, url_for
 from cs50 import SQL
@@ -30,28 +28,6 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-def hash_password(password):
-    """called when user registers"""
-    salt = os.urandom(16)
-    hash_value = hashlib.pbkdf2_hmac(
-        'sha256',
-        password.encode ('utf-8'),
-        salt,
-        100000
-    )
-    return salt.hex(), hash_value.hex()
-
-def verify_password(stored_salt_hex, stored_hash_hex, password_attempt):
-    """called when user logs"""
-    salt = bytes.fromhex(stored_salt_hex)
-    has_to_check = bytes.fromhex(stored_hash_hex)
-    new_hash = hashlib.pbkdf2_hmac(
-        'sha256',
-        password_attempt.encode('utf-8'),
-        salt,
-        100000
-    )
-    return new_hash == hash_to_check
 #routes
 
 #HOME
